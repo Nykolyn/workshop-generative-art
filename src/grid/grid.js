dae92671363c11a64d5c851f1e21cb3c217c6c3a
Nykolyn <nykolynandrii@gmail.com>
2021-10-31 21:57:46 +0200

    add random cirlces width, color, appearance canvas drawing

--- src/grid/grid.js
+++ src/grid/grid.js
@@ -1,5 +1,6 @@
 const canvasSketch = require('canvas-sketch');
 const random = require('canvas-sketch-util/random')
+const palettes = require('nice-color-palettes')
 
 const lerp = (min, max, t) => {
   return min * (1 - t) + max * t;
@@ -10,21 +11,26 @@ const settings = {
 };
 
 const sketch = () => {
+  const palette = random.pick(palettes)
+
   const createGrid = () => {
     const points = []
-    const count = 200;
+    const count = 40;
     for (let x = 0; x < count; x += 1) {
       for (let y = 0; y < count; y += 1) {
         const u = count <= 1 ? 0.5 : x / (count - 1)
         const v = count <= 1 ? 0.5 : y / (count - 1)
-        points.push([u, v])
+        points.push({
+          color: random.pick(palette),
+          position: [u, v],
+          radius: Math.abs(random.gaussian() * 0.01)
+        })
       }
     }
 
     return points
   }
 
-  random.setSeed(512)
   const points = createGrid().filter(() => random.value() > 0.5)
   const margin = 400;
 
@@ -32,15 +38,17 @@ const sketch = () => {
     context.fillStyle = 'white';
     context.fillRect(0, 0, width, height);
 
-    points.forEach(([u, v]) => {
+    points.forEach(({ position, radius, color }) => {
+      const [u, v] = position;
       const x = lerp(margin, width - margin, u)
       const y = lerp(margin, height - margin, v)
 
       context.beginPath()
-      context.rect(x, y, 4, 4)
+      context.arc(x, y, radius * width, 0, Math.PI * 2, false)
       context.strokeStyle = 'black'
-      context.lineWidth = 4
-      context.stroke();
+      context.lineWidth = 20
+      context.fillStyle = color
+      context.fill();
     })
   };
 };
